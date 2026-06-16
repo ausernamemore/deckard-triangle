44bdd3628da4f63d57a17f3409c66b054da297eb
Palta <lucabonagd@gmail.com>
2026-06-16 02:26:55 -0300

    created

---
+++ main.hs
@@ -0,0 +1,76 @@
+applyRule :: Int -> Int -> Int
+applyRule 1 1 = 2
+
+applyRule 1 2 = 1
+applyRule 2 2 = 3
+
+applyRule 1 3 = 3
+applyRule 2 3 = 2
+applyRule 3 3 = 1
+
+applyRule 1 4 = 0
+applyRule 2 4 = 0
+applyRule 3 4 = 0
+applyRule 4 4 = 0
+
+-- Golden trio patterns:
+    -- rainbow (repetitive): 223131
+    -- hexagon (recursive): 213321
+    -- ???????
+
+applyRule a b = applyRule b a -- make all rules commutative
+
+
+
+red    = "\ESC[41m"
+green = "\ESC[42m"
+blue  = "\ESC[44m"
+purple = "\ESC[45m"
+cyan = "\ESC[46m"
+white = "\ESC[47m"
+yellow = "\ESC[43m"
+reset  = "\ESC[0m"
+
+style :: Int -> String
+style 1 = red    ++ " " ++ reset
+style 2 = green  ++ " " ++ reset
+style 3 = blue   ++ " " ++ reset
+style 4 = purple ++ " " ++ reset
+style 5 = cyan   ++ " " ++ reset
+style 6 = yellow ++ " " ++ reset
+style 7 = white  ++ " " ++ reset
+
+-- Main stuff below!!!
+
+data Layer = Layer [Maybe Int]
+combine :: (Maybe Int, Maybe Int) -> Maybe Int
+combine (Nothing, Just 1) = Just 1
+combine (Just 1, Nothing) = Just 1
+combine (Just a, Just b) = Just $ applyRule a b
+
+next :: Layer -> Layer
+next (Layer xs) = Layer $ map combine (zip (xs++[Nothing]) (Nothing:xs))
+
+instance Show Layer where
+    show (Layer []) = ""
+    show (Layer (Nothing:xs)) = "  " ++ show (Layer xs)
+    show (Layer ((Just s):xs)) = style s ++ " " ++ show (Layer xs)
+
+showLayers :: [Layer] -> IO ()
+showLayers layers =
+    let width = (let Layer lastLayer = last layers in length lastLayer) in
+    let printLayer height [] = return ()
+        printLayer height (l:xs) = do
+                putStrLn $ concat (replicate (height - 1) " ") ++ show l
+                printLayer (height-1) xs
+    in printLayer width layers
+
+extendN :: Int -> Layer -> [Layer]
+extendN 0 _ = error "Size can't be zero"
+extendN 1 x = [x]
+extendN n x = x : extendN (n-1) (next x)
+
+base = Layer [Just 1]
+
+main = showLayers $ extendN 80 base
+
